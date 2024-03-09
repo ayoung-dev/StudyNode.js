a47ad0505645209330834a828ccc00c7bffbe53c
a-young0210 <dkdudab@hanmail.net>
2024-03-09 21:09:08 +0900

    ~ chapter 3.5

---
+++ example/exec.js
@@ -0,0 +1,13 @@
+const { ChatCompletionResponseMessageRoleEnum } = require('openai');
+
+const exec = require('child_process').exec;
+
+const process = exec('ls');
+
+process.stdout.on('data', function(data) {
+    console.log(data.toString());
+});
+
+process.stderr.on('data', function(data) {
+    console.error(data.toString());
+});
